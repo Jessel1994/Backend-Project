05ad85c53b7edd6dbf35680ec6dc47c37c54adfb
Jessel1994 <jesseldhokia@gmail.com>
2023-10-03 11:39:02 +0100

    git sta

--- models/topics.model.js
+++ models/topics.model.js
@@ -2,11 +2,7 @@ const db = require('../db/connection')
 
 exports.selectTopics = () => {
     return db.query(`SELECT * FROM topics`).then((result) => {
-<<<<<<< HEAD
-        
-=======
     
->>>>>>> 3-get-api
         return result.rows
     })
 }
