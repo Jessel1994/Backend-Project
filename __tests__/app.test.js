5e4afde540291f5bf4f6d65ca8dbac86ea22c51e
Jessel1994 <jesseldhokia@gmail.com>
2023-10-04 15:51:14 +0100

    task 6 get comments by article ID

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -151,7 +151,7 @@ describe('GET /api/articles/:article_id/comments', () => {
         .get('/api/articles/1/comments')
         .expect(200)
         .then(({ body }) => {
-            console.log(body.comments)
+            
             expect(Array.isArray(body.comments)).toBe(true)
             if (body.comments.length !== 0) {
                 body.comments.forEach((comment) => {
